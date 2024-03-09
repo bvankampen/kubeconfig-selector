4b19cff637eee8ab087bf57858883a8ee18c423a
Bas van Kampen <bas@ping6.nl>
2024-03-09 10:52:13 +0100

    Refatoring

---
+++ internal/selector/types.go
@@ -0,0 +1,36 @@
+package selector
+
+import (
+	"github.com/rivo/tview"
+	"github.com/urfave/cli"
+	"k8s.io/client-go/tools/clientcmd/api"
+)
+
+type TableListItem struct {
+	Field string
+	Value string
+}
+
+type ConfigList struct {
+	Rows           []TableListItem
+	Config         api.Config
+	RedactedConfig api.Config
+	Context        *api.Context
+}
+
+type Selector struct {
+	ctx          *cli.Context
+	appConfig    AppConfig
+	kubeConfigs  []api.Config
+	activeConfig api.Config
+	app          *tview.Application
+	list         *tview.List
+	table        *tview.Table
+	configView   *tview.TextView
+	debugView    *tview.TextView
+	helpView     *tview.TextView
+	tableRow     int
+	tableColumn  int
+	configList   []ConfigList
+	debug        bool
+}

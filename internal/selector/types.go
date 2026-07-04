4f3596acc73bf3cc8718dccadf16c52c488d3567
Bas van Kampen <bas@ping6.nl>
2026-07-05 01:17:45 +0200

    chore: remove dead types in selector/types.go (#45)

--- internal/selector/types.go
+++ internal/selector/types.go
@@ -3,25 +3,10 @@ package selector
 import (
 	"github.com/bvankampen/kubeconfig-selector/internal/config"
 	"github.com/urfave/cli"
-	"k8s.io/client-go/tools/clientcmd/api"
 )
 
-type TableListItem struct {
-	Field string
-	Value string
-}
-
-type ConfigList struct {
-	Rows           []TableListItem
-	Config         api.Config
-	RedactedConfig api.Config
-	Context        *api.Context
-}
-
 type Selector struct {
-	ctx          *cli.Context
-	appConfig    config.AppConfig
-	kubeConfigs  []api.Config
-	activeConfig api.Config
-	debug        bool
+	ctx       *cli.Context
+	appConfig config.AppConfig
+	debug     bool
 }
